b3d137d828034cf93906005822b0a436e6b30439
Sumin13 <su184913@icloud.com>
2024-01-17 22:00:30 +0000

    16/01/24

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -55,7 +55,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
-  console.log("LOG USER IN COMING SOON");
+  req.session.loggedIn = ture;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
