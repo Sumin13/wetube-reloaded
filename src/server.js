e77beecfc7cffb1823835b6f42fc975cc732981d
Sumin13 <su184913@icloud.com>
2024-01-18 21:20:20 +0000

    18/01/24

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -15,12 +16,17 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: "hello", resave: true, saveUninitialized: true }));
 
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
-    next();
-  });
-});
+console.log(process.env.COOKIE_SECRET);
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
 
 app.use(localsMiddleware);
 app.use("/", rootRouter);
